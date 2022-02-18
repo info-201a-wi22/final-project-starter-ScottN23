15b7cac1bf7a933218334a8bf671e0f36c0c8c01
ScottN23 <scottn523@gmail.com>
2022-02-18 14:58:54 -0800

    Updated select & wranglement

--- source/Chart 1.R
+++ source/Chart 1.R
@@ -5,14 +5,13 @@ COVID19_data <- read.csv("../data/COVID19_daily_survey.csv", header = TRUE, stri
 
 # Filter columns that are of use
 COVID19_data_cleaned <- COVID19_data %>%
-  select(unique_id, covid_status, TST, sleepdiary_fellasleep, socialize_min, exercise, stress, isolation, worry_scale)
+  select(unique_id, covid_status, socialize_min)
 
 # Data Wrangling: Remove NA's from columns in use and turn 0s and 1s to true/false
 COVID19_data_cleaned <- COVID19_data_cleaned %>%
   group_by(covid_status) %>%
   filter(!is.na(covid_status), !is.na(socialize_min)) %>%
-  mutate(covid_status = covid_status == 1) %>%
-  mutate(exercise = exercise == 1)
+  mutate(covid_status = covid_status == 1)
 
 # Data Visualization: correlation between COVID-19 and a person's physical health
 covid_on_physical <- ggplot(COVID19_data_cleaned, aes(x = covid_status, y = socialize_min)) + 
