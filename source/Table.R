7a572dfd7ecfb98a9a64cb6b0c14749b35404fa2
peijie36 <peijiezheng2001@gmail.com>
2022-02-18 16:35:06 -0800

    Update aggregate table

--- source/Table.R
+++ source/Table.R
@@ -1,14 +1,19 @@
-library(tidyverse)
+library(dplyr)
 
 COVID19_data <- read.csv("../data/COVID19_daily_survey.csv", header = TRUE, stringsAsFactors = FALSE)
 
-useful_data <- COVID19_data %>% 
+aggregate_table <- COVID19_data %>% 
   select(unique_id, covid_status, TST, sleepdiary_fellasleep, socialize_min, exercise, stress, isolation, worry_scale) %>%
+  mutate(covid_status = covid_status == 1) %>%
+  filter(!is.na(covid_status)) %>%
   group_by(covid_status) %>%
-  mutate(covid_status = covid_status == 1, exercise = exercise == 1) %>%
-  filter(!is.na(covid_status), !is.na(socialize_min)) %>% 
-  rename(total_sleep_time = TST, difficulty_falling_asleep = sleepdiary_fellasleep, virtually_socialized_min = socialize_min) %>%
-  mutate(total_sleep_time = round(total_sleep_time, digits = 1))
+  summarize(total_sleep_time = round(mean(TST, na.rm = TRUE), digits = 3), 
+            difficulty_falling_asleep = round(mean(sleepdiary_fellasleep, na.rm = TRUE), digits = 3), 
+            virtually_socialization_min = round(mean(socialize_min, na.rm = TRUE), digits = 3),
+            stress_level = round(mean(stress, na.rm = TRUE), digits = 3),
+            prop_exercise = round(mean(exercise, na.rm = TRUE), digits = 3),
+            isolation_level = round(mean(isolation, na.rm = TRUE), digits = 3),
+            worry_level = round(mean(worry_scale, na.rm =TRUE), digits = 3))
 
-table <- useful_data
-View(table)
+
+View(aggregate_table)
