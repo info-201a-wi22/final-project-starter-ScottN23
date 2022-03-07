cf4a090bf16e38bc1abab0f6b2be89ad55be7bc1
jndoan <jamesdoan123@gmail.com>
2022-03-06 16:42:58 -0800

    Introductory updates

--- source/app_ui.R
+++ source/app_ui.R
@@ -12,7 +12,22 @@ source("Socialize Chart.R")
 source("mental_health_chart.R")
 
 ui <- navbarPage(
+  
   title = "Can't Sleep Through The Pandemic",
+  tabPanel("Introduction to Can't Sleep Through The Pandemic",
+           fluidRow(
+             column(3,
+                    img(src = "https://media.giphy.com/media/kaIJGG8lNC3u5DM3tg/giphy.gif"),
+                    ),
+             column(3,
+                    img(src = "https://media.giphy.com/media/Zm17XBTlVfjacXg44z/giphy.gif"),
+                    ),
+             column(6,
+                    h1("Introduction to Project:"),
+                    strong("Contemporarily...,"),
+                    ),
+           ),
+  ),
   virtual_social_chart,
   mental_health_chart
 )
